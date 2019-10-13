a2475e308192fd59eaab5af92705275404d51562
Hazel Ilkar <hazelilkar@hazel-ilkar.attlocal.net>
2019-10-12 19:32:25 -0700

    2ND COMMIT2ND COMMIT2ND COMMIT2ND COMMIT2ND COMMIT2ND COMMIT2ND COMMIT2ND COMMIT2ND COMMIT2ND COMMIT2ND COMMIT2ND COMMIT2ND COMMIT2ND COMMIT2ND COMMIT2ND COMMIT2ND COMMIT2ND COMMIT2ND COMMIT2ND COMMIT2ND COMMIT2ND COMMIT

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
-const express = require('express')
-const server = express()
+var express = require('express');
+var app = express();
 
-/* route requests for static files to appropriate directory */
-server.use('/public', express.static(__dirname + '/public'))
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
 
+// set the view engine to ejs
+app.set('view engine', 'ejs');
 
+// make express look in the public directory for assets (css/js/img)
+app.use(express.static(__dirname + '/public'));
 
-/* final catch-all route to index.html defined last */
-server.get('/*', (req, res) => {
-  res.sendFile(__dirname + '/public/main.html');
-})
+// set the home page route
+app.get('/', function(req, res) {
 
-const port = 5000;
-server.listen(port, function() {
-  console.log('server listening on port ' + port)
+    // ejs render automatically looks in the views folder
+    res.render('index');
+});
+
+app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
 });
